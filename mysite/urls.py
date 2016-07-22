c835b79e35ca6decc61368631dbe5bff30773182
Mateusz Szymański <mateusz_szymanski@interia.pl>
2016-07-22 15:23:12 +0200

    Addition login page and possibility to logout. Only logged user can change and modify posts. Some changes in css file

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,8 +1,15 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+import django.contrib.auth.views
+
+
+admin.autodiscover()
+
 urlpatterns = [
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include('blog.urls')),
+    url(r'^accounts/login/$', django.contrib.auth.views.login, name='login'),
+    url(r'^accounts/logout/$', django.contrib.auth.views.logout, name='logout', kwargs={'next_page': '/'}),
 ]
